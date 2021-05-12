9ce427c87de5ff7d319bffbd02d1d78463082ab0
MariaJoseCandia <mariajosecandia100@gmail.com>
2021-05-12 15:37:33 -0300

    Initial commit

---
+++ cuenta/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cuenta
+{
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public double Efectivo { get; set; }
+        public double Saldo { get; set; }
+        public int Cbu { get; set; } = 0;
+        
+       
+        public Cuenta Cuenta { get; set; }
+
+        public Cliente()
+        {
+            Cuenta = new Cuenta();
+            Efectivo = 0;
+            Saldo = 0;
+        }
+
+        public void Acreditar(double monto)
+        {
+            Saldo = Saldo + monto * 0.8;
+            Cuenta.Acreditar(monto * 0.2);
+        }
+        public void Debitar(double monto)
+        {
+            Saldo = Saldo - monto * 0.8;
+            Cuenta.Debitar(monto * 0.2);
+        }
+
+        public bool TieneAlMenos(double monto) => Saldo >= monto;
+    }
+}
---
+++ cuenta/Cuenta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cuenta
+{
+    class Cuenta
+    {
+        private int Contador { get; set; } = 0;
+        public int Cbu { get; set; }
+        public double Saldo { get; set; }
+       
+        public Cuenta()
+        {
+            Cbu = ++Contador;
+            Saldo = 0;
+        }
+
+        public void Acreditar(double monto) => Saldo += monto;
+        public void Debitar(double monto) => Saldo -= monto;
+
+
+    }
+}
