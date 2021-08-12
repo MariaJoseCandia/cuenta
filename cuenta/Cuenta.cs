ab56183fb0536eab70cf053ac198261096fa5086
MariaJoseCandia <mariajosecandia100@gmail.com>
2021-08-12 15:46:12 -0300

    Update Cuenta.cs

--- cuenta/Cuenta.cs
+++ cuenta/Cuenta.cs
@@ -12,12 +12,15 @@ namespace cuenta
        
         public Cuenta()
         {
-            Cbu = ++Contador;
+            Contador++;
+            Cbu=Contador;
             Saldo = 0;
         }
 
         public void Acreditar(double monto) => Saldo += monto;
         public void Debitar(double monto) => Saldo -= monto;
+
+        public bool TieneAlMenos(double monto) => Saldo >= monto;
 
 
     }
